f115476a86a9d779e4cc09e7e78a5e497245a325
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-09-25 17:39:23 +0300

    fixes component folder spelling, add framer  motion plugin, add react-type-writer plugin

---
+++ Components/BackgroundCircles.tsx
@@ -0,0 +1,17 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+export const BackgroundCircles = (props: Props) => {
+	return (
+		<div className={"relative flex justify-center items-center"}>
+			<div className={"border border-red-500 rounded-full h-[200px] w-[200px] mt-52 animate-ping"}/>
+			<div/>
+			<div/>
+			<div/>
+			<div/>
+		</div>
+	);
+};
