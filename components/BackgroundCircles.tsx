ea1411439820cea14ec230c6310c59a9d0c89bf3
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-09-26 08:14:18 +0300

    remove opacity property from circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -26,13 +26,13 @@ export const BackgroundCircles = (props: Props) => {
 				className={"absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping"}
 			/>
 			<div
-				className={"absolute border rounded-full border-[#333333] h-[300px] w-[300px] mt-52 opacity-20"}
+				className={"absolute border rounded-full border-[#333333] h-[300px] w-[300px] mt-52"}
 			/>
 			<div
-				className={"absolute border rounded-full border-[#333333] h-[500px] w-[500px] mt-52 opacity-20"}
+				className={"absolute border rounded-full border-[#333333] h-[500px] w-[500px] mt-52"}
 			/>
 			<div
-				className={"absolute border rounded-full border-[#F7AB0A] opacity-20 h-[650] w-[650] mt-52 animate-pulse"}
+				className={"absolute border rounded-full border-[#F7AB0A] h-[650] w-[650] mt-52 animate-pulse"}
 			/>
 			<div
 				className={"absolute border rounded-full border-[#F7AB0A] opacity-20 h-[800] w-[800] mt-52"}
