64018c371743fa0a4e7d6188d57558ecb81cf97a
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-09-27 09:58:19 +0300

    completes about section

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,8 @@ import profilePic2 from "../public/images/daphine_andrew.jpg";
 
 type Props = {
     
-};
+}
+
 export const About = (props: Props) => {
     return (
         <motion.div
@@ -19,7 +20,7 @@ export const About = (props: Props) => {
             <h3 className={"absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl"}>
                 About
             </h3>
-             <motion.div
+            {/* <motion.div
                 initial={{
                     x:-200,
                     opacity:0
@@ -41,8 +42,25 @@ export const About = (props: Props) => {
                     alt={'Me and My wife'}
                     className={"flex-shrink-0 w-56  object-cover md:w-64 xl:w-[500px]  md:rounded-lg  rounded-full"}
                 />
-            </motion.div>
-
+            </motion.div>*/}
+            <motion.img
+                initial={{
+                    x:-200,
+                    opacity:0
+                }}
+                transition={{
+                    duration:1.2
+                }}
+                whileInView={{
+                    x:0,
+                    opacity:1
+                }}
+                viewport={{
+                    once:true
+                }}
+                src="/images/daphine_andrew.jpg" alt="This is me an dmy wife"
+                className={"-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 sm:w-60 sm:w-full md:w-64 md:h-95  xl:w-[500px] xl:h-[600px]  object-cover rounded-full md:rounded-lg"}
+            />
             <div className={"space-y-10 px-0 md:px-10"}>
                 <h4 className={"text-3xl font-semibold"}>
                     Here is a <span className={"underline decoration-[#F7AB0A]/40"}>little</span> background
