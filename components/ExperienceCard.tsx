b444bf4d7a651da5b90f6544a88dac58a5536c46
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-09-27 13:17:25 +0300

    adds scaling to experienceCard

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -7,24 +7,22 @@ type Props = {
 };
 export const ExperienceCard = (props: Props) => {
 	return (
-		<article className={"flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden"}>
+		<article className={"flex flex-col rounded-lg items-center space-y-1 flex-shrink-0 w-[500px] md:w-[600px]" +
+			" xl:w-[900px] snap-center bg-[#292929] p-10 opacity-40 scale-90 hover:opacity-100 hover:scale-100" +
+			" cursor-pointer" +
+			" transition-all duration-200 overflow-hidden"}>
 			<motion.img
-				initial={{
-					y:-100,
-					opacity:0
-				}}
+				initial={{y:-100,opacity:0}}
 				transition={{duration:1.2}}
 				whileInView={{opacity:1, y:0}}
 				viewport={{once:true}}
-				className={"w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px] object-cover" +
-					" object-center"}
+				className={"w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"}
 				src="/images/amazon-icon.svg" alt="The amazon image"
 			/>
 			<div className={"px-10 md:px-10"}>
 				<h4 className={"text-4xl font-light"}>CEO of PAPAFAM</h4>
 				<p className={"font-bold text-2xl mt-1"}>PAPAFAM</p>
 				<div className={"flex space-x-2 my-2"}>
-					
 					<img
 						className={"h-10 w-10 rounded-full bg-amber-100 border border-amber-100"}
 						src="/images/javascript-1.svg" alt="javascript icon"
